7f877ed6067ee9192e9ede3ab32fbfb32838c09a
Marcus Widegren <marcuswidegren123@gmail.com>
2025-04-06 12:45:47 +0200

    add encrypted data

---
+++ translations.js
@@ -0,0 +1,29 @@
+// Translations for wedding invitation
+const translations = {
+  en: {
+    passwordPrompt: "Wedding Invitation",
+    enterPassword: "Please enter the password to view our invitation:",
+    submit: "Submit",
+    incorrectPassword: "Incorrect password. Please try again.",
+    decryptError: "Error decrypting data. Please try again.",
+    weddingTitle: "We're Getting Married",
+    joinUs: "Please join us to celebrate our wedding",
+    ceremony: "Ceremony",
+    reception: "Reception",
+    rsvpPrefix: "RSVP by",
+    lookForward: "We look forward to celebrating this special day with you!"
+  },
+  sv: {
+    passwordPrompt: "Bröllopsinbjudan",
+    enterPassword: "Ange lösenordet för att se vår inbjudan:",
+    submit: "Skicka",
+    incorrectPassword: "Felaktigt lösenord. Försök igen.",
+    decryptError: "Fel vid dekryptering av data. Försök igen.",
+    weddingTitle: "Vi ska gifta oss",
+    joinUs: "Vi bjuder in er att fira vårt bröllop",
+    ceremony: "Vigsel",
+    reception: "Bröllopsfest",
+    rsvpPrefix: "OSA senast",
+    lookForward: "Vi ser fram emot att fira denna speciella dag med er!"
+  }
+};
